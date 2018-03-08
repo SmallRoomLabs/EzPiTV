09f545e98c7ba981033329f1823769ca2882e0f6
SmallRoomLabs <git@smallroomlabs.com>
2018-03-08 20:27:57 +0100

    Added checks for missing file

--- drawscreen.sh
+++ drawscreen.sh
@@ -8,19 +8,30 @@ _dt=""
 _mod=""
 
 while true; do
-	dt=$(env TZ=CET date +%H:%M)
-	mod=$(stat -c %Y $file)
+  # If there's no file then try again a bit later
+  if [ ! -f $file ]; then
+    sleep 0.5
+    continue
+  fi
 
-	if [ "$dt" == "$_dt" ] && [ "$mod" == "$_mod" ]; then
-		sleep 0.5
-		continue
-	fi
+  # Get the modification time of the file for comparison
+  mod=$(stat -c %Y $file)
+  # Get the surrent hour and minutes for displaying the time
+  dt=$(env TZ=CET date +%H:%M)
 
-	_mod=$mod
-	_dt=$dt
+  # Have the file or time changed since last time?
+  if [ "$dt" == "$_dt" ] && [ "$mod" == "$_mod" ]; then
+    sleep 0.5
+    continue
+  fi
 
-	convert -pointsize 60 -fill white \
-        	-stroke black -strokewidth 6 -draw "text 920,50 \"$dt\"" \
-        	-stroke none                 -draw "text 920,50 \"$dt\"" \
-		$file - | ./ppmto565 > /dev/fb0
+  # Yes, new info to display
+  _mod=$mod
+  _dt=$dt
+
+  # Add the time to the image, convert to 556 and send to framebuffer
+  convert -pointsize 60 -fill white \
+          -stroke black -strokewidth 6 -draw "text 920,50 \"$dt\"" \
+          -stroke none                 -draw "text 920,50 \"$dt\"" \
+          $file - | ./ppmto565 > /dev/fb0
 done
